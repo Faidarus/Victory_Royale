6d1a80c46b79f3629467005055735664297f05bc
Ubuntu <ubuntu@ip-172-31-42-164.eu-west-2.compute.internal>
2021-05-04 14:13:25 +0000

    Test.

--- jenkins/scripts/test_repos.sh
+++ jenkins/scripts/test_repos.sh
@@ -1,14 +1,11 @@
 frontend="spring-petclinic-angular/angular.json"
+sudo chown -R $USER /usr/local/lib/node_modules
 sudo apt-get update
-#sudo apt-get install curl
-#curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
-#sudo npm link @angular/cli
 npm install --global yarn
 npm install -g @angular/cli@latest 
 npm install --save-dev @angular-devkit/build-angular
 npm install -i
-#sudo npm install --save-dev @angular-devkit/build-angular
 cd spring-petclinic-angular
 ng test #${frontend}
 ng e2e #${frontend}
