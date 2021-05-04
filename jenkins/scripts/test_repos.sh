81ffa9d7b5c90185e47a60be4c88d0b14aa7946f
Ubuntu <ubuntu@ip-172-31-42-164.eu-west-2.compute.internal>
2021-05-04 12:43:44 +0000

    Test.

--- jenkins/scripts/test_repos.sh
+++ jenkins/scripts/test_repos.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 sudo apt-get install curl
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm link @angular/cli
-npm install --global yarn
+sudo npm link @angular/cli
+sudo npm install --global yarn
 ng test
 ng e2e
 ng serve
