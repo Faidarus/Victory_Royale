7393cd5bd377fe6d58eac0358742ee6973d29b43
Ubuntu <ubuntu@ip-172-31-42-164.eu-west-2.compute.internal>
2021-05-04 13:00:40 +0000

    Test.

--- jenkins/scripts/remove_repos.sh
+++ jenkins/scripts/remove_repos.sh
@@ -1,2 +1,9 @@
-rm -r spring-petclinic-angular
-rm -r spring-petclinic-rest
+DIR="/spring-petclinic-angular/"
+if [ -d "$DIR" ]; then
+    rm -r $DIR
+fi
+
+DIR="/spring-petclinic-rest/"
+if [ -d "$DIR" ]; then
+    rm -r $DIR
+fi 
