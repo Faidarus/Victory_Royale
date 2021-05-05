e04839bf3d1b3465a71f2735c688782da844ace8
jmiller2612 <jmiller2612@gmail.com>
2021-05-05 11:01:34 +0100

    fixed paths

--- jenkins/scripts/build_images.sh
+++ jenkins/scripts/build_images.sh
@@ -2,7 +2,9 @@ rm spring-petclinic-rest/src/main/resources/application.properties
 rm spring-petclinic-rest/src/main/resources/application-mysql.properties
 mv Docker/application-mysql.properties spring-petclinic-rest/src/main/resources/
 mv Docker/application.properties spring-petclinic-rest/src/main/resources/
-cd kubernetes
+echo "stuff here"
 ls
+echo "stuff in petclinic rest"
+ls /spring-petclinic-rest
 docker-compose build
-cd ..
+ls
