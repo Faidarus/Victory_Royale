516b5f9b1bccaa6154e760bfcbf781a3965e2515
jmiller2612 <jmiller2612@gmail.com>
2021-05-05 10:23:14 +0100

    changed docker-compose. added kubernetes sh

--- jenkins/scripts/build_images.sh
+++ jenkins/scripts/build_images.sh
@@ -1,3 +1,7 @@
+rm spring-petclinic-rest/src/main/resources/application.properties
+rm spring-petclinic-rest/src/main/resources/application-mysql.properties
+mv kubernetes/application-mysql.properties spring-petclinic-rest/src/main/resources/
+mv kubernetes/application.properties spring-petclinic-rest/src/main/resources/
 cd kubernetes
 docker-compose build
 cd ..
---
+++ jenkins/scripts/kubernetes_run.sh
@@ -0,0 +1,10 @@
+#some aws stuff here, access key and region etc
+
+#make kubectl
+cd kubernetes
+kubectl apply -f backend/backend.yaml
+kubectl apply -f frontend/frontend.yaml
+kubectl apply -f nginx/nginx.yaml
+
+kubectl get service
+kubectl get pod
