cd279ee5439b35789f43c43a3480ba079c1c3640
jmiller2612 <jmiller2612@gmail.com>
2021-05-05 10:27:13 +0100

    changed docker-compose. added kubernetes sh

--- jenkins/scripts/build_images.sh
+++ jenkins/scripts/build_images.sh
@@ -1,3 +1,4 @@
+ls
 rm spring-petclinic-rest/src/main/resources/application.properties
 rm spring-petclinic-rest/src/main/resources/application-mysql.properties
 mv ../kubernetes/application-mysql.properties spring-petclinic-rest/src/main/resources/
