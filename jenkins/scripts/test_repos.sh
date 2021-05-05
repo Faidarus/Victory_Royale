9fd8da84647c82d4132e6d1586bae7c8d350d501
Ubuntu <ubuntu@ip-172-31-42-164.eu-west-2.compute.internal>
2021-05-05 08:15:46 +0000

    npm added.

--- jenkins/scripts/test_repos.sh
+++ jenkins/scripts/test_repos.sh
@@ -2,6 +2,7 @@ frontend="spring-petclinic-angular/angular.json"
 sudo chown -R $USER:$(id -gn $USER) /home/jenkins/.config
 sudo apt update -y
 sudo apt install -y nodejs
+sudo apt install -y npm
 npm install --global yarn
 npm install -g @angular/cli@latest 
 npm install --save-dev @angular-devkit/build-angular
