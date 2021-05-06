ee0376cff142c2ee2676daae2c1daefeb8854f5f
jmiller2612 <jmiller2612@gmail.com>
2021-05-06 19:09:09 +0100

    jenkins update

--- terraform/production.tf
+++ terraform/production.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-west-2"
-  access_key = "AKIAVMSWB4DS2GBKEINJ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
