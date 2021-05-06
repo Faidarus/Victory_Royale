f2b399a57239b03a8c804524c08dfda5ed9afe2c
jmiller2612 <jmiller2612@gmail.com>
2021-05-06 11:45:08 +0100

    kube

--- jenkins/scripts/kubernetes_run.sh
+++ jenkins/scripts/kubernetes_run.sh
@@ -4,8 +4,7 @@
 cd kubernetes
 kubectl apply -f backend/backend.yaml
 kubectl apply -f frontend/frontend.yaml
-kubectl apply -f nginx/nginx.yaml
 kubectl apply -f nginx/configmap.yaml
-
+kubectl apply -f nginx/nginx.yaml
 kubectl get service
 kubectl get pod
