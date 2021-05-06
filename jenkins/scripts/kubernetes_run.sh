ec3dbd03104cebf5d6be76ebbb382f8eda1fd165
jmiller2612 <jmiller2612@gmail.com>
2021-05-06 20:40:04 +0100

    jenkins update

--- jenkins/scripts/kubernetes_run.sh
+++ jenkins/scripts/kubernetes_run.sh
@@ -4,6 +4,7 @@ aws eks --region eu-west-2 update-kubeconfig --name vrcluster
 #make kubectl
 cd kubernetes
 
+kubectl delete all --all
 kubectl apply -f backend/backend.yaml
 kubectl apply -f frontend/frontend.yaml
 kubectl apply -f nginx/nginx_tom.yaml
