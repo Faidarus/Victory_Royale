88fe7dd728588e283fde8b54748631670790a47a
Ubuntu <ubuntu@ip-172-31-12-77.eu-west-1.compute.internal>
2021-05-06 09:32:13 +0000

    added outputs for rds

--- terraform/output.tf
+++ terraform/output.tf
@@ -9,3 +9,21 @@ output "kubeconfig-certificate-authority-data" {
 output "eks_cluster_certificate_authority" {
   value = aws_eks_cluster.ekscluster.certificate_authority
 }
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.vrpets.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.vrpets.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.vrpets.username
+  sensitive   = true
+}
